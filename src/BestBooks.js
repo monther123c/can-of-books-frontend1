73e5e2b74772098b94bd79418d62e4541778fbec
monther123c <monthertamimi123@gmail.com>
2022-09-04 20:45:10 +0300

    front

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -7,6 +8,20 @@ class BestBooks extends React.Component {
       books: []
     }
   }
+  componentDidMount = () =>{
+    axios
+    .get(`http://localhost:3001/Books`)
+    .then(result =>{
+    
+        this.setState({
+          books:result.data
+        })
+      
+      })    
+      .catch(err =>{
+        console.log(err);
+      })
+    }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
@@ -16,13 +31,23 @@ class BestBooks extends React.Component {
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+         <>
+         <h3> {this.state.books[0].title}</h3>
+          <p >{this.state.books[0].description}</p>
+          <h3 >{this.state.books[0].states}</h3>
+          </>
+        
+          <>
+         <h3> {this.state.books[1].title}</h3>
+          <p >{this.state.books[1].description}</p>
+          <h3 >{this.state.books[1].states}</h3>
+          </>
+          <>
+         <h3> {this.state.books[2].title}</h3>
+          <p >{this.state.books[2].description}</p>
+          <h3 >{this.state.books[2].states}</h3>
+          </>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
       </>
     )
   }
